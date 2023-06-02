5e6763a525e2ae778e15407df060716d84a6ce8a
Ibrahim Ismayilov <i.ismayilov@EDU.epsb.ca>
2023-06-02 11:22:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,43 @@ dataStr = file.read()
 file.close()
 
 color_data = json.loads(dataStr)
+
+
+# # 1
+# for i in range(len(color_data)):
+#     print("\n" + color_data[i]["name"])
+#     print(color_data[i]["family"])
+
+# # 2
+# for i in range(len(color_data)):
+#     if color_data[i]["brightness"] > 200:
+#         print("\n" + color_data[i]["name"])
+#         print(color_data[i]["family"])
+
+# # 3
+# count = 0
+# for i in range(len(color_data)):
+#     if color_data[i]["family"] == "Red" or color_data[i]["family"] == "Pink":
+#         count += 1
+# print(f"\n{count}")
+
+#4
+# count = 0
+# color_family_input = input("Please enter a color family: ")
+# for i in range(len(color_data)):
+#     if color_data[i]["family"].lower() == color_family_input.lower():
+#         print("\n" + color_data[i]["name"])
+#         print(color_data[i]["family"])
+#         count += 1
+# print("\n" + count)
+
+# 5
+color_letter_input = input("Please enter a letter: ")
+count = 0
+for i in range(len(color_data)):
+    if color_data[i]["family"][0].lower() == color_letter_input.lower():
+        print("\n" + color_data[i]["name"])
+        print(color_data[i]["family"])
+        count += 1
+
+print("\n" + str(count))
